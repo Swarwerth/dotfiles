fd12d8e30617646f023695d97091e865d9443aac
Clément Nguyen <clement.nguyen@epita.fr>
2025-08-13 18:57:40 +0200

    feat(nvim): update lazygit

---
+++ .config/nvim/lua/config/options.lua
@@ -0,0 +1,16 @@
+-- Options are automatically loaded before lazy.nvim startup
+-- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
+-- Add any additional options here
+
+local opt = vim.opt
+
+opt.relativenumber = false
+
+opt.tabstop = 4 -- TAB = 4 spaces
+opt.expandtab = true -- Insert spaces on pressing the TAB key
+opt.softtabstop = 4 -- Number of spaces inserted instead of a TAB character
+opt.shiftwidth = 4 -- Number of spaces inserted when indenting
+
+opt.spelllang = { "en", "fr" }
+
+opt.sidescrolloff = 2 -- Columns of context
